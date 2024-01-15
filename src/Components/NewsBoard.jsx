100362631c64de16575af5eed57b5e4ced924950
prathip <prathip@stridus.com>
2024-01-15 21:30:39 +0530

    first changes

---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+
+const NewsBoard = ({category}) => {
+
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles));
+  }, [category]);
+
+  return (
+    <div>
+      <h2 className="text-center">
+        Latest <span className="badge bg-danger">News</span>
+      </h2>
+      <div className="d-flex flex-wrap">
+        {articles.map((news, index) => (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NewsBoard;
---
+++ src/Components/NewsItem.jsx
@@ -0,0 +1,42 @@
+import defaultImage from "../assets/news.jpg";
+import { useState } from "react";
+
+const NewsItem = ({ title, description, src, url }) => {
+  const [imageError, setImageError] = useState(false);
+
+  function handleImageError() {
+    // You can do additional error handling logic here if needed
+    // For now, simply set the imageError flag to true
+    setImageError(true);
+  }
+
+  return (
+    <div
+      className="card bg-dark text-light d-inline-block m-3 p-2"
+      style={{ maxWidth: "265px", minWidth: "265px" }}
+    >
+      <img
+        onError={handleImageError}
+        src={imageError ? defaultImage : src ? src : defaultImage}
+        className="card-img-top"
+        alt="..."
+        style={{ height: "120px", width: "calc(100% - 1px)" }}
+      />
+      <div className="card-body">
+        <h5 className="card-title" style={{ minHeight: "70px" }}>
+          {title.slice(0, 50)}
+        </h5>
+        <p className="card-text" style={{ minHeight: "96px" }}>
+          {description
+            ? description.slice(0, 90)
+            : "News having no description"}
+        </p>
+        <a href={url} className="btn btn-primary">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
